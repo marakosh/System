c47914d79f2e435c5d0617715405d9809fcc7c07
marakosh <marat.garifulin160@gmail.com>
2023-02-20 23:01:34 +0400

    HW

---
+++ System 1/System 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+ProcessStartInfo startInfo = new ProcessStartInfo();
+startInfo.FileName = "childProcess.exe";
+
+Process childProcess = new Process();
+childProcess.StartInfo = startInfo;
+
+Console.WriteLine("Запуск дочернего процесса...");
+childProcess.Start();
+
+childProcess.WaitForExit();
+
+Console.WriteLine($"Дочерний процесс завершен с кодом {childProcess.ExitCode}");
